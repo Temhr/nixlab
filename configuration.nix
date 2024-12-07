74da85dc9e1cf7573235379b1c8fdb95e8c025f0
Temhr <gebrehiwet@gmail.com>
2024-12-07 16:29:37 -0500

    added nix linter

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alejandra
     git
     github-desktop
     keepassxc
