6c7a5b05d81a217e4fb706c7c5520e6eca42444a
Temhr <gebrehiwet@gmail.com>
2024-12-07 16:21:59 -0500

    fixed pkg name

--- configuration.nix
+++ configuration.nix
@@ -105,7 +105,7 @@
     kdePackages.partitionmanager
     fastfetch
     nfs-utils
-    nvim
+    neovim
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
