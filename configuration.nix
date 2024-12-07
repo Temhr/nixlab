73e7b741d9dc49d305159db94ed37a8f7710200c
Temhr <gebrehiwet@gmail.com>
2024-12-07 16:18:48 -0500

    added nvim package

--- configuration.nix
+++ configuration.nix
@@ -99,12 +99,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  git
-  github-desktop
-  keepassxc
-  kdePackages.partitionmanager
-  fastfetch
-  nfs-utils
+    git
+    github-desktop
+    keepassxc
+    kdePackages.partitionmanager
+    fastfetch
+    nfs-utils
+    nvim
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
