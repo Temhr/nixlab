e939364e07d2ade7838fef5f7e7b6c86fe37041e
Temhr <gebrehiwet@gmail.com>
2024-12-09 08:23:38 -0500

    cleaned up config

--- configuration.nix
+++ configuration.nix
@@ -243,11 +243,7 @@
     dates = "*-*-* 02:00:00";
     options = "--delete-older-than 3d";
   };
-  # Optimize storage
-  # You can also manually optimize the store via:
-  #    nix-store --optimise
-  # Refer to the following link for more details:
-  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  # Optimize storage for incoming new files
   nix.settings.auto-optimise-store = true;
 
   # This value determines the NixOS release from which the default
