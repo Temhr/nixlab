f27278e35bbb29a0e62c51daf872df281679c7ff
Temhr <gebrehiwet@gmail.com>
2024-12-17 09:20:22 -0500

    reorganised for clarity

--- flake.nix
+++ flake.nix
@@ -10,13 +10,13 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixtop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+    nixosConfigurations = {
+      nixtop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
     };
   };
 }
