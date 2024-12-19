7a8927addd0c07fa77bbbbc922f7b16e78df189d
Temhr <gebrehiwet@gmail.com>
2024-12-19 04:32:19 -0500

    Update configuration.nix

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -128,8 +128,6 @@
   users.users = {
     # FIXME: Replace with your username
     temhr = {
-      # TODO: You can set an initial password for your user. If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install. Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
