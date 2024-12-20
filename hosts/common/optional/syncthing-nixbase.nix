416185b6b6a5aad778c2a1392bb0f3a401a3a183
Temhr <gebrehiwet@gmail.com>
2024-12-20 18:27:12 -0500

    fixes

--- hosts/common/optional/syncthing-nixbase.nix
+++ hosts/common/optional/syncthing-nixbase.nix
@@ -27,7 +27,7 @@
       # all machines must have same declaration, but "devices" reflect the opposite device
       folders = {
         # Name of folder in Syncthing, also the folder ID
-        "nixbase-home" = {
+        "home-nixbase" = {
           # Which folder to add to Syncthing
           path = "/home/temhr";
           # Which devices to share the folder with
