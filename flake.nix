765c134143848ddb81de6dc8182d0ca12a661492
Temhr <gebrehiwet@gmail.com>
2024-12-24 10:08:14 -0500

    unify temhr home-manager across hosts

--- flake.nix
+++ flake.nix
@@ -61,15 +61,7 @@
 
     # Standalone home-manager configuration entrypoint. Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "temhr@nixbase" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main home-manager configuration file <
-          ./home-manager/home.nix
-        ];
-      };
-      "temhr@nixtop" = home-manager.lib.homeManagerConfiguration {
+      "temhr" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
