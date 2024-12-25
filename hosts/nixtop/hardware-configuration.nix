6b3f9ee8ad5ba27a5c8e46b06a3368c7d8729c1d
Temhr <gebrehiwet@gmail.com>
2024-12-25 01:33:07 -0500

    added shelf drive

--- hosts/nixtop/hardware-configuration.nix
+++ hosts/nixtop/hardware-configuration.nix
@@ -29,6 +29,11 @@
       fsType = "ext4";
     };
 
+  fileSystems."/home/temhr/mnt/shelf" =
+    { device = "/dev/disk/by-uuid/39d1a447-348b-4d86-9104-c4bae37c46fc";
+      fsType = "ext4";
+    };
+
   fileSystems."/mnt/hdd-r0" =
     { device = "192.168.0.210:/hdd-r0";
       fsType = "nfs";
