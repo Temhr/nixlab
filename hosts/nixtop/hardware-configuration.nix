a2ecfbfcff22c4fd31959bd8a3cc9afd9a1bdd4a
Temhr <gebrehiwet@gmail.com>
2024-12-25 04:36:24 -0500

    Update hardware-configuration.nix

--- hosts/nixtop/hardware-configuration.nix
+++ hosts/nixtop/hardware-configuration.nix
@@ -29,11 +29,11 @@
       fsType = "ext4";
     };
 
-  fileSystems."/home/temhr/mnt/shelf" =
+  fileSystems."/home/temhr/shelf" =
     { device = "/dev/disk/by-uuid/39d1a447-348b-4d86-9104-c4bae37c46fc";
       fsType = "ext4";
     };
-  systemd.tmpfiles.rules = [ "d /home/temhr/mnt/shelf 1777 root root " ];
+  systemd.tmpfiles.rules = [ "d /home/temhr/shelf 1777 root root " ];
 
   fileSystems."/mnt/hdd-r0" =
     { device = "192.168.0.210:/hdd-r0";
