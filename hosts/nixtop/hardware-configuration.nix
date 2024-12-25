bb48f56bc0e946091c567aa4e57a10413ef3a3fc
Temhr <gebrehiwet@gmail.com>
2024-12-25 02:29:16 -0500

    Update hardware-configuration.nix

--- hosts/nixtop/hardware-configuration.nix
+++ hosts/nixtop/hardware-configuration.nix
@@ -32,7 +32,7 @@
   fileSystems."/home/temhr/mnt/shelf" =
     { device = "/dev/disk/by-uuid/39d1a447-348b-4d86-9104-c4bae37c46fc";
       fsType = "ext4";
-      options = [ "mode=777" "gid=001" ];
+      options = [ "rw" "umask=001" ];
     };
 
   fileSystems."/mnt/hdd-r0" =
