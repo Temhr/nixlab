ebed1a16cec5fedee43177eb04b2b0e415c01a8e
Temhr <gebrehiwet@gmail.com>
2024-12-25 15:53:15 -0500

    Update default.nix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,6 +1,23 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
 {
-  # List your module files here
-  module1 = import ./module1.nix;
+  imports = [
+    # Paths to other modules.
+    # Compose this module out of smaller ones.
+    ./module1.nix
+  ];
+
+  options = {
+    # Option declarations.
+    # Declare what settings a user of this module can set.
+    # Usually this includes a global "enable" option which defaults to false.
+  };
+
+  config = {
+    # Option definitions.
+    # Define what other settings, services and resources should be active.
+    # Usually these depend on whether a user of this module chose to "enable" it
+    # using the "option" above.
+    # Options for modules imported in "imports" can be set here.
+  };
 }
