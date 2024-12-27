782ab67621477bd978fbaf1216ab64ac60a43320
Temhr <gebrehiwet@gmail.com>
2024-12-27 09:01:02 -0500

    Update configuration.nix

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -127,12 +127,6 @@
     scons  #Improved, cross-platform substitute for Make
     python3Full  #High-level dynamically-typed programming language
 
-    ## Browsers
-    brave  #Privacy-oriented browser for Desktop and Laptop computers
-    #microsoft-edge  #The web browser from Microsoft
-    #google-chrome  #Freeware web browser developed by Google
-    #vivaldi  #A Browser for our Friends, powerful and personal
-
     ## Communication
     discord  #All-in-one cross-platform voice and text chat for gamers
     obs-studio  #Free and open source software for video recording and live streaming
