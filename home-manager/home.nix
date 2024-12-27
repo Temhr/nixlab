3abe5635b9ece35e340b152a9f1a05703e949a98
Temhr <gebrehiwet@gmail.com>
2024-12-27 08:49:20 -0500

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -62,6 +62,7 @@
 
   #Browsers
   chrome.enable = true; #enables Chrome browser
+  vivaldi.enable = false; #enables Chrome browser
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
