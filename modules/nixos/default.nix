cfd5f1936d88a38c11b01ceaa7a0b84bef6e8f45
Temhr <gebrehiwet@gmail.com>
2024-12-28 14:23:31 -0500

    Update default.nix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,9 +3,7 @@
     # Paths to other modules.
     # Compose this module out of smaller ones.
     ./games.nix
-    ./graphical-shells.nix
     ./media.nix
-    ./syncthing.nix
     ./terminal-emulators.nix
   ];
 
