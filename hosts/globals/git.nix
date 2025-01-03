82d35f53c0f85e94db3576630a5d71b9a23a1518
Temhr <gebrehiwet@gmail.com>
2025-01-03 03:44:15 -0500

    git fix

--- hosts/globals/git.nix
+++ hosts/globals/git.nix
@@ -1,12 +1,21 @@
 { config, lib, pkgs, ... }: {
 
-  environment.systemPackages = with pkgs; [
 
-    pkgs.github-desktop #GUI for managing Git and GitHub
-    pkgs.git-credential-keepassxc #Helper that allows Git (and shell scripts) to use KeePassXC as credential store
+  options = {
+      github-desktop = {
+          enable = lib.mkEnableOption "enables github-desktop";
+      };
+  };
 
+  config = lib.mkMerge [
+      (lib.mkIf config.github-desktop.enable {
+        environment.systemPackages = [ pkgs.github-desktop ]; #GUI for managing Git and GitHub
+      })
   ];
 
+  environment.systemPackages = with pkgs; [
+    git-credential-keepassxc #Helper that allows Git (and shell scripts) to use KeePassXC as credential store
+  ];
   programs.git.enable = true;  #Distributed version control system
 
 }
