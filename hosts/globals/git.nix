368b9fe1779c8586fc3805142edd678d755c8769
Temhr <gebrehiwet@gmail.com>
2025-01-03 04:01:19 -0500

    Update git.nix

--- hosts/globals/git.nix
+++ hosts/globals/git.nix
@@ -20,7 +20,7 @@
       (lib.mkIf config.git-credential-keepassxc.enable {
         environment.systemPackages = [ pkgs.git-credential-keepassxc ]; #Helper that allows Git (and shell scripts) to use KeePassXC as credential store
       })
-      (lib.mkDefault 'gitFull'; programs.git.package;)  #Distributed version control system
+      (lib.mkDefault true; programs.chromium.enable;)  #Distributed version control system
 
   ];
 
