42264736173cd24ea00ebd5b94da4c95e3df9b33
Temhr <gebrehiwet@gmail.com>
2025-01-04 23:50:10 -0500

    incus attempt

--- modules/nixos/virtualizations.nix
+++ modules/nixos/virtualizations.nix
@@ -38,6 +38,48 @@
           virtualisation.incus.enable = true;
           networking.nftables.enable = true;
           environment.systemPackages = [ pkgs.incus ];  #Powerful system container and virtual machine manager
+          #preseed provides a basic directory backed storage pool and a bridged NAT network with DHCP
+          virtualisation.incus.preseed = {
+            networks = [
+              {
+                config = {
+                  "ipv4.address" = "10.0.100.1/24";
+                  "ipv4.nat" = "true";
+                };
+                name = "incusbr0";
+                type = "bridge";
+              }
+            ];
+            profiles = [
+              {
+                devices = {
+                  eth0 = {
+                    name = "eth0";
+                    network = "incusbr0";
+                    type = "nic";
+                  };
+                  root = {
+                    path = "/";
+                    pool = "default";
+                    size = "35GiB";
+                    type = "disk";
+                  };
+                };
+                name = "default";
+              }
+            ];
+            storage_pools = [
+              {
+                config = {
+                  source = "/var/lib/incus/storage-pools/default";
+                };
+                driver = "dir";
+                name = "default";
+              }
+            ];
+          };
+
+          networking.firewall.trustedInterfaces = [ "incusbr0" ];
         })
         (lib.mkIf config.podman.enable {
           environment.systemPackages = [ pkgs.podman ];  #A program for managing pods, containers and container images
