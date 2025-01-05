542f866df2a4dacc39f9872d9c56ed86498430ae
Effy <9110264+Temhr@users.noreply.github.com>
2025-01-05 17:16:06 -0500

    nixser stuff

---
+++ hosts/nixser/additional-drives.nix
@@ -0,0 +1,28 @@
+
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  fileSystems."/home" =
+    { device = "/dev/disk/by-label/home";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mirror" =
+    { device = "/dev/disk/by-uuid/8c3b9f97-d5c5-4434-a2f6-6c12c4dc6ab3";
+      fsType = "ext4";
+    };
+
+#  fileSystems."/mnt/hdd-r0" =
+#    { device = "192.168.0.210:/hdd-r0";
+#      fsType = "nfs";
+#      options = [
+#        "x-systemd.automount" "noauto"
+#        "x-systemd.idle-timeout=60" # disconnects after 60 seconds
+#      ];
+#    };
+
+#  systemd.tmpfiles.rules = [
+#    "d /mirror 1770 root root "
+#    "d /mnt 1770 root root "
+#  ];
+}
