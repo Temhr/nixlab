49538de5430cc65a02b3805c06fd12ffdc4b1155
Temhr <temhr4@gmail.com>
2025-01-05 05:13:56 -0500

    Update configuration.nix

--- hosts/nixbase/configuration.nix
+++ hosts/nixbase/configuration.nix
@@ -87,19 +87,6 @@
   # TODO: Set your hostname
   networking.hostName = "nixbase";
 
-  networking = {
-    useDHCP = false;
-    defaultGateway = "192.168.0.1";
-    nameservers = ["192.168.0.1" "8.8.8.8"];
-    interfaces.br0.ipv4.addresses = [ {
-        address = "192.168.0.201";
-        prefixLength = 24;
-      } ];
-    bridges = {
-      br0 = { interfaces = [ "wlo1" ]; };
-    };
-  };
-
   ## Enable Syncthing (only for this host)
   #syncace.enable = true;
   syncbase.enable = true;
