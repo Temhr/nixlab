dc01ae0ce78fb31c57834d456c00e600e542f992
Temhr <9110264+Temhr@users.noreply.github.com>
2025-01-06 09:38:56 -0500

    fix

--- hosts/nixbase/additional-drives.nix
+++ hosts/nixbase/additional-drives.nix
@@ -38,8 +38,9 @@
     mountdPort = 4002;
     statdPort = 4000;
     extraNfsdConfig = '''';
-  }
+  };
   networking.firewall = {
     allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
-    allowedUDPPorts = [ 111  2049 4000 4001 4002 20048 ]; };
+    allowedUDPPorts = [ 111  2049 4000 4001 4002 20048 ];
+  };
 }
