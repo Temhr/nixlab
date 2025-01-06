8350d24e8990367355b56fa75b39b00e1ecdf467
Temhr <9110264+Temhr@users.noreply.github.com>
2025-01-06 09:37:59 -0500

    more nfs firewall adjusting

--- hosts/nixbase/additional-drives.nix
+++ hosts/nixbase/additional-drives.nix
@@ -30,7 +30,16 @@
     "d /mnt 1744 temhr users "
   ];
 
-  services.nfs.server.enable = true;
-  services.nfs.server.exports = '' /mirror 192.168.0.0/255.255.255.0(rw,no_root_squash,fsid=0,no_subtree_check) '';
-  networking.firewall = { allowedTCPPorts = [ 2049 ];  allowedUDPPorts = [ 2049 ]; };
+  services.nfs.server = {
+    enable = true;
+    exports = '' /mirror 192.168.0.0/255.255.255.0(rw,no_root_squash,fsid=0,no_subtree_check) '';
+    # fixed rpc.statd port; for firewall
+    lockdPort = 4001;
+    mountdPort = 4002;
+    statdPort = 4000;
+    extraNfsdConfig = '''';
+  }
+  networking.firewall = {
+    allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
+    allowedUDPPorts = [ 111  2049 4000 4001 4002 20048 ]; };
 }
