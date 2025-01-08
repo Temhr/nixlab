cc3ffaa3be80a9d0910e336fc2061de3bcf9aa14
Temhr <gebrehiwet@gmail.com>
2025-01-08 00:13:07 -0500

    cron -> sytemd

--- hosts/globals/auto-pull.nix
+++ hosts/globals/auto-pull.nix
@@ -1,11 +1,19 @@
-{ config, ... }: {
+{ config, lib, pkgs, ... }: {
 
-  # Enable cron service
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "*/5 * * * * root /home/temhr/nixlab/bin/auto-pull.sh"
-    ];
+  systemd.timers.git-pull = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "1min";
+      OnUnitActiveSec = "5min";
+      Unit = "git-pull.service";
+    };
   };
-
+  systemd.services.git-pull = {
+    description = "Git Repository Update Service";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/home/temhr/nixlab/bin/auto-pull.sh";
+      User = "temhr";
+    };
+  };
 }
