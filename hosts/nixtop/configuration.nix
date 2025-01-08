e1e556a0a27388981f4481275d7132d9a5503a9a
Temhr <gebrehiwet@gmail.com>
2025-01-07 22:04:20 -0500

    standardizing options

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -80,8 +80,9 @@
     settings.auto-optimise-store = true;
   };
 
-  ## Bootloader - systemd-boot
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   ## Limit the number of generations to present
   boot.loader.systemd-boot.configurationLimit = 10;
 
