e2c7562b99afcf3bc77f229793a4794202b1945e
Temhr <gebrehiwet@gmail.com>
2025-01-14 06:37:34 -0500

    auto-build reset

--- hosts/globals/auto-build.nix
+++ hosts/globals/auto-build.nix
@@ -3,16 +3,16 @@
   system.autoUpgrade = {
     enable = true;
     operation = "switch"; #switch or boot
-    flake = "github:Temhr/nixlab"; #Flake URI of the NixOS configuration to build
-    #flake = "path:${rootPath}";  #local repo
+    #flake = "github:Temhr/nixlab"; #Flake URI of the NixOS configuration to build
+    flake = "path:${rootPath}";  #local repo
     allowReboot = false;
     #randomizedDelaySec = "5m";
-    dates = "06:05";
-    #flags = [
-    #  "--update-input"
-    #  "nixpkgs"
-    #  "-L"  # print build logs
+    dates = "06:33";
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L"  # print build logs
     #  "--no-write-lock-file"  # don't write to the lock file
-    #];
+    ];
   };
 }
--- hosts/globals/default.nix
+++ hosts/globals/default.nix
@@ -3,7 +3,7 @@
     # Paths to other modules.
     # Compose this module out of smaller ones.
     ./audio.nix
-    ./auto-build.nix
+    #./auto-build.nix
     ./auto-pull.nix
     ./git.nix
     ./graphical-shells.nix
