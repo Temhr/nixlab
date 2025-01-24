af8edd32e005d58174508a6e2858aa0ebd601bb7
Effy <9110264+Temhr@users.noreply.github.com>
2025-01-23 19:36:39 -0500

    Update hardware-configuration.nix

--- hosts/nixser/hardware-configuration.nix
+++ hosts/nixser/hardware-configuration.nix
@@ -14,12 +14,12 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-label/root";
+    { device = "/dev/disk/by-uuid/2fc510d4-38c2-4277-9d88-82f521dad7f3";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-label/boot";
+    { device = "/dev/disk/by-uuid/76A5-F070";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
