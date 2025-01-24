9f21d46bc4cdd3d7068fdc359bf446902f16b538
Effy <9110264+Temhr@users.noreply.github.com>
2025-01-23 19:35:17 -0500

    Update hardware-configuration.nix

--- hosts/nixace/hardware-configuration.nix
+++ hosts/nixace/hardware-configuration.nix
@@ -19,7 +19,7 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-label/EFI";
+    { device = "/dev/disk/by-label/boot";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
