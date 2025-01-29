322f96ef9cc85ff908318b0fa63c4a126cbeacab
Temhr <gebrehiwet@gmail.com>
2025-01-29 17:09:22 -0500

    Update auto-move-files.nix

--- hosts/globals/auto-move-files.nix
+++ hosts/globals/auto-move-files.nix
@@ -4,7 +4,7 @@ let
     MoveBash = pkgs.writeShellScript "MoveBash.sh" ''
        cp /home/temhr/nixlab/bin/bash/.bash_profile /home/temhr/.bash_profile &&
        cp /home/temhr/nixlab/bin/bash/.bashrc /home/temhr/.bashrc &&
-       cp /home/temhr/nixlab/bin/bash/.bash/bash_aliases /home/temhr/.bash/bash_prompt &&
+       cp /home/temhr/nixlab/bin/bash/.bash/bash_aliases /home/temhr/.bash/bash_aliases &&
        cp /home/temhr/nixlab/bin/bash/.bash/bash_functions /home/temhr/.bash/bash_functions &&
        cp /home/temhr/nixlab/bin/bash/.bash/bash_prompt /home/temhr/.bash/bash_prompt
     '';
