b718ee8f482d6280ef2bc4d5cd7efe50ec223612
Temhr <temhr4@gmail.com>
2025-01-29 14:02:40 -0500

    Update configuration.nix

--- hosts/nixser/configuration.nix
+++ hosts/nixser/configuration.nix
@@ -20,7 +20,6 @@
 
     # You can also split up your configuration and import pieces of it here:
     ../globals
-    ../globals/auto-flake-push.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
