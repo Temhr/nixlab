2066b8b91bc81080bcbed5eaf9757c3f494a9b1b
Temhr <temhr4@gmail.com>
2025-02-04 02:55:28 -0500

    Update backup-home.sh

--- bin/backup-home.sh
+++ bin/backup-home.sh
@@ -8,9 +8,9 @@ mirrorDir="/mirror"
 mntDir="/mnt/mirser"
 
 if [ -d "$mirrorDir" ]; then
-    rsync -rva --delete --exclude '.cache/' /home/temhr/ ${mirrorDir}/home/${hostname}/ && echo "hello"
+    rsync -rva --delete --exclude '.cache/' /home/temhr/ ${mirrorDir}/home/${hostname}/ && echo "hello" > /home/temhr/hello
 else
-    rsync -rva --delete --exclude '.cache/' /home/temhr/ ${mntDir}/home/${hostname}/ && echo "howdy"
+    rsync -rva --delete --exclude '.cache/' /home/temhr/ ${mntDir}/home/${hostname}/ && echo "howdy" > /home/temhr/howdy
 fi
 
 ## Exit on Success
