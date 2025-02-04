92312cad0ed3d747e6fb0a10dfe3c0c008e608eb
Temhr <temhr4@gmail.com>
2025-02-04 02:38:31 -0500

    Update auto-backup-home.nix

--- hosts/globals/auto-backup-home.nix
+++ hosts/globals/auto-backup-home.nix
@@ -7,7 +7,7 @@ in
   systemd.services.backup-home = {
     description = "script write";
     serviceConfig = {
-      ExecStart = "${pkgs.rsync}" backuphomeShellScript;
+      ExecStart = "${pkgs.rsync} ${backuphomeShellScript}";
       Type = "oneshot";
       User = "root";
     };
