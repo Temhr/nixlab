d227b2e69a1bec5750b597e3892b16a5d2b30275
Temhr <temhr4@gmail.com>
2025-02-05 16:55:29 -0500

    priority differential backups; else full backups

--- bin/backup-home.sh
+++ bin/backup-home.sh
@@ -9,11 +9,23 @@ serDir="/mnt/mirser"
 baseDir="/mnt/mirbase"
 
 if [ -d "$mirrorDir" ]; then
-    /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${mirrorDir}/home/${hostname}/
+    if [ -d "${mirrorDir}/home/${hostname}/" ]; then
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${mirrorDir}/home/${hostname}/ /home/temhr/ ${mirrorDir}/home/${hostname}/
+    else
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${mirrorDir}/home/${hostname}/
+    fi
 elif [ -d "$serDir" ]; then
-    /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${serDir}/home/${hostname}/
+    if [ -d "$serDir/home/${hostname}/" ]; then
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${serDir}/home/${hostname}/ /home/temhr/ ${serDir}/home/${hostname}/
+    else
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${serDir}/home/${hostname}/
+    fi
 else
-    /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${baseDir}/home/${hostname}/
+    if [ -d "${baseDir}/home/${hostname}/" ]; then
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${baseDir}/home/${hostname}/ /home/temhr/ ${baseDir}/home/${hostname}/
+    else
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${baseDir}/home/${hostname}/
+    fi
 fi
 
 ## Exit on Success
