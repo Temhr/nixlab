aeec5309febe4dec3236c871a7176672ffc5e5d9
Temhr <9110264+Temhr@users.noreply.github.com>
2025-03-10 10:19:14 -0400

    Update firefox.nix

--- hosts/common/firefox.nix
+++ hosts/common/firefox.nix
@@ -25,6 +25,7 @@
       };
       OverrideFirstRunPage = "";  #blank if you want to disable the first run page
       OverridePostUpdatePage = "";  #blank if you want to disable the post-update page
+      RequestedLocales = "en-US";
       SearchBar = "unified"; # alternative: "separate"
 
       /* ---- EXTENSIONS ---- */
