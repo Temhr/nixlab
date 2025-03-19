6e4cbe1ed16f4272bf3a380fe46f5f5ed40d5367
Temhr <9110264+Temhr@users.noreply.github.com>
2025-03-19 15:34:35 -0400

    Update virtualizations.nix

--- modules/nixos/virtualizations.nix
+++ modules/nixos/virtualizations.nix
@@ -62,45 +62,8 @@
           virtualisation.incus.enable = true;
           networking.nftables.enable = true;
           users.users.temhr.extraGroups = ["incus-admin"];
-          virtualisation.incus.preseed = {
-            networks = [
-              {
-                config = {
-                  "ipv4.address" = "10.0.100.1/24";
-                  "ipv4.nat" = "true";
-                };
-                name = "incusbr0";
-                type = "bridge";
-              }
-            ];
-            profiles = [
-              {
-                devices = {
-                  eth0 = {
-                    name = "eth0";
-                    network = "incusbr0";
-                    type = "nic";
-                  };
-                  root = {
-                    path = "/";
-                    pool = "default";
-                    size = "35GiB";
-                    type = "disk";
-                  };
-                };
-                name = "default";
-              }
-            ];
-            storage_pools = [
-              {
-                config = {
-                  source = "/var/lib/incus/storage-pools/default";
-                };
-                driver = "dir";
-                name = "default";
-              }
-            ];
-          };
+          # minimal incus initialization below
+          # $ incus admin init --minimal
           networking.firewall.trustedInterfaces = [ "incusbr0" ];
         })
         (lib.mkIf config.podman.enable {
