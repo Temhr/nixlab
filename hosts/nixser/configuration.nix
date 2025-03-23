25d8ae18cb1fcc3560e6e14608eafbcc78700838
Temhr <9110264+Temhr@users.noreply.github.com>
2025-03-23 10:29:18 -0400

    Update configuration.nix

--- hosts/nixser/configuration.nix
+++ hosts/nixser/configuration.nix
@@ -124,10 +124,10 @@
   ## Virtualizations
   #bottles.enable = true;    #Easy-to-use wineprefix manager
   #distrobox.enable = true;    #Wrapper around podman or docker to create and start containers
-  incus.enable = true;   #Powerful system container and virtual machine manager
+  #incus.enable = true;   #Powerful system container and virtual machine manager
   #podman.enable = true;    #A program for managing pods, containers and container images
-  quickemu.enable = true;    #Quickly create and run optimised Windows, macOS and Linux virtual machines
-  virt-manager.enable = true;    #Desktop user interface for managing virtual machines
+  #quickemu.enable = true;    #Quickly create and run optimised Windows, macOS and Linux virtual machines
+  #virt-manager.enable = true;    #Desktop user interface for managing virtual machines
   #wine.enable = true;    #Open Source implementation of the Windows API on top of X, OpenGL, and Unix
 
   ## Art Dev Tools
