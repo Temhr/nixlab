02a78032f281cab5619a0cbd50164fd8432ed8b3
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-06 07:31:17 -0400

    templating

--- lib/option-select.nix
+++ lib/option-select.nix
@@ -1,11 +1,11 @@
 { config, lib, ... }:
 
 let
-  cfg = config.nameSubject;
+  cfg = config.group;
 in {
   options = {
-    nameSubject = {
-      nameChoice = lib.mkOption {
+    group = {
+      choice = lib.mkOption {
         type = lib.types.enum [ "none" "one" "two" "three" ];
         default = "none";
         description = "Select which between three options or none";
@@ -14,18 +14,18 @@ in {
   };
 
   config = lib.mkMerge [
-    (lib.mkIf (cfg.nameChoice == "one") {
+    (lib.mkIf (cfg.choice == "one") {
 
     })
-    (lib.mkIf (cfg.nameChoice == "two") {
+    (lib.mkIf (cfg.choice == "two") {
 
     })
-    (lib.mkIf (cfg.nameChoice == "three") {
+    (lib.mkIf (cfg.choice == "three") {
 
     })
   ];
 }
 
-##Goes in config file
-# Choose between "none" "one" "two" "three"
-# nameSubject.nameChoice = "two";
+## Goes in config file
+# Choose between these choices: "none" "one" "two" "three"
+# group.choice = "two";
