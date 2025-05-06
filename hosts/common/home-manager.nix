0eee13708168cee4649dbea579003e9004a1ada6
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-06 17:06:19 -0400

    home manager refactor

--- hosts/common/home-manager.nix
+++ hosts/common/home-manager.nix
@@ -7,7 +7,7 @@
     extraSpecialArgs = { inherit inputs outputs; };
     users = {
       # Import your home-manager configuration
-      temhr = import ../../home-manager/home.nix;
+      temhr = import ../../home/temhr.nix;
     };
   };
 }
