98dd2535872e675faa6b163f6831f9fa79f0abae
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-07 14:50:40 -0400

    open ssh refactor

--- hosts/common/core/users/users.nix
+++ hosts/common/core/users/users.nix
@@ -1,13 +1,11 @@
 { pkgs, ... }: {
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  # Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
-    # FIXME: Replace with your username
     temhr = {
-      # TODO: You can set an initial password for your user. If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install. Be sure to change it (using passwd) after rebooting!
       #initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [ ]; # Add your SSH public key(s) here, if you plan on using SSH to connect
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKITqIX76nKk6GvwM//USjaBD+YruF7YiTJxMNXUXVu2 temhr" ]; # Add your SSH public key(s) here, if you plan on using SSH to connect
+      # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       #"adbusers" "kvm" => adb and android deveopment
       #"video" "render" => Blender user development
       extraGroups = [ "root" "wheel" "networkmanager" "adbusers" "kvm" "video" "render"];
@@ -18,6 +16,7 @@
     guest = {
       isNormalUser = true;
       initialPassword = "";
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKITqIX76nKk6GvwM//USjaBD+YruF7YiTJxMNXUXVu2 temhr" ]; # Add your SSH public key(s) here, if you plan on using SSH to connect
     };
   };
 }
