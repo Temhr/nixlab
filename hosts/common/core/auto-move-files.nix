e01c3580599f0a039cc5fa35624915facc442897
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-07 12:07:46 -0400

    refactor home files

--- hosts/common/core/auto-move-files.nix
+++ hosts/common/core/auto-move-files.nix
@@ -1,18 +1,15 @@
 { pkgs, ... }:
 let
     MoveFilesShellScript = pkgs.writeShellScript "auto-move-files.sh" ''
-       cp /home/temhr/nixlab/bin/bash/.bash_profile /home/temhr/.bash_profile &&
-       cp /home/temhr/nixlab/bin/bash/.bashrc /home/temhr/.bashrc &&
-       cp /home/temhr/nixlab/bin/bash/.bash/bash_aliases /home/temhr/.bash/bash_aliases &&
-       cp /home/temhr/nixlab/bin/bash/.bash/bash_functions /home/temhr/.bash/bash_functions &&
-       cp /home/temhr/nixlab/bin/bash/.bash/bash_prompt /home/temhr/.bash/bash_prompt &&
-       cp /home/temhr/nixlab/bin/bash/.bash/emoticons /home/temhr/.bash/emoticons &&
-       cp /home/temhr/nixlab/bin/bash/.bash/environment_variables /home/temhr/.bash/environment_variables &&
-       cp /home/temhr/nixlab/bin/bash/.bash/ghostty_theme_randomizer /home/temhr/.bash/ghostty_theme_randomizer &&
-       cp /home/temhr/nixlab/bin/bash/.bash/ghostty_themes.txt /home/temhr/.bash/ghostty_themes.txt &&
-       cp /home/temhr/nixlab/bin/config/kscreenlockerrc /home/temhr/.config/kscreenlockerrc &&
-       cp /home/temhr/nixlab/bin/config/powerdevilrc /home/temhr/.config/powerdevilrc
-
+       cp /home/temhr/nixlab/home/temhr/files/bash/.bash_profile /home/temhr/.bash_profile &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/.bashrc /home/temhr/.bashrc &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/bash_aliases /home/temhr/.bash/bash_aliases &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/bash_functions /home/temhr/.bash/bash_functions &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/bash_prompt /home/temhr/.bash/bash_prompt &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/emoticons /home/temhr/.bash/emoticons &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/environment_variables /home/temhr/.bash/environment_variables &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/ghostty_theme_randomizer /home/temhr/.bash/ghostty_theme_randomizer &&
+       cp /home/temhr/nixlab/home/temhr/files/bash/ghostty_themes.txt /home/temhr/.bash/ghostty_themes.txt
     '';
 in
 {
