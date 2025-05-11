e120af0c552f0c203f37d2b0a548f445a70e8246
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-10 20:58:24 -0400

    all hardwares

--- hardware/zb15g2-k1.nix
+++ hardware/zb15g2-k1.nix
@@ -1,7 +1,15 @@
-# This is your system's configuration file. Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 { ... }: {
   imports = [
-
+    ./common/global
+    ./common/optional
   ];
+
+  # Choose between these choices: "none" "k" "p"
+  driver-nvidia.quadro = "k";
+
+  mount-home.enable = true; #mounts home drive
+  mount-mirror.enable = true; #mounts mirror drive
+  #mount-mirk1.enable = true; #mounts mirk1 nfs
+  mount-mirk3.enable = true; #mounts mirk3 nfs
 
 }
--- hardware/zb17g2-k5.nix
+++ hardware/zb17g2-k5.nix
@@ -7,9 +7,9 @@
   # Choose between these choices: "none" "k" "p"
   driver-nvidia.quadro = "k";
 
-  mount-home.enable = true;
-  #mount-mirror.enable = true;
-  mount-mirk1.enable = true;
-  mount-mirk3.enable = true;
+  mount-home.enable = true; #mounts home drive
+  #mount-mirror.enable = true; #mounts mirror drive
+  mount-mirk1.enable = true; #mounts mirk1 nfs
+  mount-mirk3.enable = true; #mounts mirk3 nfs
 
 }
