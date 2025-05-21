4367ce4ae0bfe8719f5bc9ca229c7ee9dc693744
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-21 09:07:51 -0400

    disko

--- hardware/zb17g2-k5.nix
+++ hardware/zb17g2-k5.nix
@@ -5,6 +5,7 @@
     #./common/optional/drives.nix
     #./common/optional/drives-additional.nix
     ./zb17g2-k5/disko-impermanence.nix
+    ./zb17g2-k5/boilerplate.nix
   ];
 
   # Choose between these choices: "none" "k" "p"
