edd858a3fd648a6a9340c3ae5f890eda9d385e81
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-21 09:37:52 -0400

    Update zb17g2-k5.nix

--- hardware/zb17g2-k5.nix
+++ hardware/zb17g2-k5.nix
@@ -1,11 +1,11 @@
 { ... }: {
   imports = [
-    #./common/global
+    ./common/global
     ./common/optional
-    #./common/optional/drives.nix
-    #./common/optional/drives-additional.nix
-    ./zb17g2-k5/disko-impermanence.nix
-    ./zb17g2-k5/boilerplate.nix
+    ./common/optional/drives.nix
+    ./common/optional/drives-additional.nix
+    #./zb17g2-k5/disko-impermanence.nix
+    #./zb17g2-k5/boilerplate.nix
   ];
 
   # Choose between these choices: "none" "k" "p"
