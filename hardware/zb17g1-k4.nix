0cbe6c21a9fffedae7ced680bfbcb8e3a5addead
Temhr <9110264+Temhr@users.noreply.github.com>
2025-05-29 12:47:22 -0400

    Update zb17g1-k4.nix

--- hardware/zb17g1-k4.nix
+++ hardware/zb17g1-k4.nix
@@ -10,7 +10,7 @@
   driver-nvidia.quadro = "k";
 
   mount-home.enable = true; #mounts home drive
-  #mount-shelf.enable = true; #mounts shelf drive in home directory
+  mount-shelf.enable = true; #mounts shelf drive in home directory
   #mount-mirror.enable = true; #mounts mirror drive
   mount-mirk1.enable = true; #mounts mirk1 nfs
   mount-mirk3.enable = true; #mounts mirk3 nfs
