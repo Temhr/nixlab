78e99a30c1d2b13eb8ea99aa721c4b1bc5625913
Temhr <9110264+Temhr@users.noreply.github.com>
2025-06-05 18:52:18 -0400

    Update media.nix

--- modules/nixos/media.nix
+++ modules/nixos/media.nix
@@ -65,7 +65,7 @@
             ];
             boot.kernelModules = [ "v4l2loopback" ];
             boot.extraModprobeConfig = ''
-              options v4l2loopback devices=2 video_nr=1,2 card_label="OBS Cam, Virt Cam" exclusive_caps=1
+              options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
             '';
             security.polkit.enable = true;
         })
