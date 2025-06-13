a5342e1cb5a26c661a77e42aa6e658265ab25b45
Temhr <9110264+Temhr@users.noreply.github.com>
2025-06-13 16:07:47 -0400

    Update auto-move-files.nix

--- home/temhr/auto-move-files.nix
+++ home/temhr/auto-move-files.nix
@@ -1,70 +1,41 @@
-# home.nix - Home Manager configuration for bash files
-{ config, pkgs, ... }:
+# home.nix - Home Manager configuration matching your actual file structure
+{ config, pkgs, lib, ... }:
 
 {
-  # Direct file management - files are symlinked automatically
   home.file = {
-    # Main bash configuration files
-    ".bash_profile".source = ./nixlab/home/files/bash/.bash_profile;
-    ".bashrc".source = ./nixlab/home/files/bash/.bashrc;
+    # Root level bash files
+    ".bash_profile".source = /home/temhr/nixlab/home/files/bash/.bash_profile;
+    ".bashrc".source = /home/temhr/nixlab/home/files/bash/.bashrc;
 
-    # Bash utility files in ~/.bash/ directory
-    ".bash/bash_aliases".source = ./nixlab/home/files/bash/.bash/bash_aliases;
-    ".bash/bash_functions".source = ./nixlab/home/files/bash/.bash/bash_functions;
-    ".bash/bash_prompt".source = ./nixlab/home/files/bash/.bash/bash_prompt;
-    ".bash/emoticons".source = ./nixlab/home/files/bash/.bash/emoticons;
-    ".bash/environment_variables".source = ./nixlab/home/files/bash/.bash/environment_variables;
-    ".bash/ghostty_themes.txt".source = ./nixlab/home/files/bash/.bash/ghostty_themes.txt;
+    # Files from the .bash/ subdirectory in your source
+    ".bash/bash_aliases".source = /home/temhr/nixlab/home/files/bash/.bash/bash_aliases;
+    ".bash/bash_functions".source = /home/temhr/nixlab/home/files/bash/.bash/bash_functions;
+    ".bash/bash_prompt".source = /home/temhr/nixlab/home/files/bash/.bash/bash_prompt;
+    ".bash/emoticons".source = /home/temhr/nixlab/home/files/bash/.bash/emoticons;
+    ".bash/environment_variables".source = /home/temhr/nixlab/home/files/bash/.bash/environment_variables;
+    ".bash/ghostty_themes.txt".source = /home/temhr/nixlab/home/files/bash/.bash/ghostty_themes.txt;
 
-    # Make ghostty theme randomizer executable
+    # Make the theme randomizer executable
     ".bash/ghostty_theme_randomizer" = {
-      source = ./nixlab/home/files/bash/.bash/ghostty_theme_randomizer;
+      source = /home/temhr/nixlab/home/files/bash/.bash/ghostty_theme_randomizer;
       executable = true;
     };
   };
 
-  # Alternative approach: Using programs.bash for better integration
+  # Configure bash through Home Manager for better integration
   programs.bash = {
     enable = true;
 
-    # You can also manage bash configuration directly here instead of separate files
-    # bashrcExtra = ''
-    #   # Additional bash configuration can go here
-    #   # This gets appended to the generated .bashrc
-    # '';
+    # History settings
+    historySize = 50000;
+    historyFileSize = 100000;
+    historyControl = [ "ignoredups" "ignorespace" "erasedups" ];
 
-    # profileExtra = ''
-    #   # Additional profile configuration
-    #   # This gets appended to the generated .bash_profile
-    # '';
-
-    # Shell aliases (alternative to separate bash_aliases file)
-    # shellAliases = {
-    #   ll = "ls -alF";
-    #   la = "ls -A";
-    #   l = "ls -CF";
-    # };
-
-    # History configuration
-    historySize = 10000;
-    historyFileSize = 20000;
-    historyControl = [ "ignoredups" "ignorespace" ];
-  };
-
-  # If you want to keep using your existing separate files but with some Home Manager benefits:
-  # You can create a systemd user service that runs on home-manager switch
-  systemd.user.services.bash-config-refresh = {
-    Unit = {
-      Description = "Refresh bash configuration after home-manager switch";
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash -c 'source ~/.bash_profile 2>/dev/null || true'";
-      RemainAfterExit = true;
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
+    # This will be appended to the Home Manager generated .bashrc
+    # You can remove this if your .bashrc file already handles sourcing
+    bashrcExtra = ''
+      # Your custom bashrc is already symlinked, so this is just for any additional config
+      # The symlinked .bashrc will take precedence
+    '';
   };
 }
