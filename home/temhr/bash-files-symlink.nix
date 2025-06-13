d1a39a0676c16079ea7a42d59a9bde2212954bc7
Temhr <9110264+Temhr@users.noreply.github.com>
2025-06-13 18:31:53 -0400

    Update bash-files-symlink.nix

--- home/temhr/bash-files-symlink.nix
+++ home/temhr/bash-files-symlink.nix
@@ -5,19 +5,19 @@
   home.file = {
     # Root level bash files - using relative paths from flake root
     ".bash_profile".source = lib.mkForce "${self}/home/files/bash/.bash_profile";
-    ".bashrc".source = "${self}/home/files/bash/.bashrc";
+    ".bashrc".source = lib.mkForce "${self}/home/files/bash/.bashrc";
 
     # Files from the .bash/ subdirectory in your source
-    ".bash/bash_aliases".source = "${self}/home/files/bash/.bash/bash_aliases";
-    ".bash/bash_functions".source = "${self}/home/files/bash/.bash/bash_functions";
-    ".bash/bash_prompt".source = "${self}/home/files/bash/.bash/bash_prompt";
-    ".bash/emoticons".source = "${self}/home/files/bash/.bash/emoticons";
-    ".bash/environment_variables".source = "${self}/home/files/bash/.bash/environment_variables";
-    ".bash/ghostty_themes.txt".source = "${self}/home/files/bash/.bash/ghostty_themes.txt";
+    ".bash/bash_aliases".source = lib.mkForce "${self}/home/files/bash/.bash/bash_aliases";
+    ".bash/bash_functions".source = lib.mkForce "${self}/home/files/bash/.bash/bash_functions";
+    ".bash/bash_prompt".source = lib.mkForce "${self}/home/files/bash/.bash/bash_prompt";
+    ".bash/emoticons".source = lib.mkForce "${self}/home/files/bash/.bash/emoticons";
+    ".bash/environment_variables".source = lib.mkForce "${self}/home/files/bash/.bash/environment_variables";
+    ".bash/ghostty_themes.txt".source = lib.mkForce "${self}/home/files/bash/.bash/ghostty_themes.txt";
 
     # Make the theme randomizer executable
     ".bash/ghostty_theme_randomizer" = {
-      source = "${self}/home/files/bash/.bash/ghostty_theme_randomizer";
+      source = lib.mkForce "${self}/home/files/bash/.bash/ghostty_theme_randomizer";
       executable = true;
     };
   };
