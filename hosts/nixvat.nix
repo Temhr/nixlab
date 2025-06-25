5ffc11ebd1ab490895e8cfadc67085c913e8485f
Temhr <9110264+Temhr@users.noreply.github.com>
2025-06-25 19:14:24 -0400

    haos network attempt

--- hosts/nixvat.nix
+++ hosts/nixvat.nix
@@ -77,6 +77,15 @@
   #virt-manager.enable = true;    #Desktop user interface for managing virtual machines
   #wine.enable = true;    #Open Source implementation of the Windows API on top of X, OpenGL, and Unix
 
+
+  networking.useNetworkd = true;  # Recommended over NetworkManager for Incus bridges
+  networking.interfaces.br0.useDHCP = true;
+  networking.bridges.br0.interfaces = [ "wlo1" ];
+  # Optional: disable DHCP on the physical NIC itself
+  networking.interfaces.wlo1.useDHCP = false;
+  # Open port for Home Assistant
+  networking.firewall.allowedTCPPorts = [ 8123 ];
+
   ## Art Dev Tools
   blender.enable = true;    #3D Creation/Animation/Publishing System
   #darktable.enable = true;    #Virtual lighttable and darkroom for photographers
