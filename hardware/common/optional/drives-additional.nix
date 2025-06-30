3b919f2726a96cc5568fd882c879ada4e3b3f15c
Temhr <9110264+Temhr@users.noreply.github.com>
2025-06-29 20:59:48 -0400

    default folder fix

--- hardware/common/optional/drives-additional.nix
+++ hardware/common/optional/drives-additional.nix
@@ -46,16 +46,6 @@
               fsType = "ext4";
               options = [ "defaults" "user" "auto" ];
             };
-        environment.sessionVariables = {
-            XDG_DESKTOP_DIR = "$HOME/shelf/Desktop";
-            XDG_DOCUMENTS_DIR = "$HOME/shelf/Documents";
-            XDG_DOWNLOAD_DIR = "$HOME/shelf/Downloads";
-            XDG_MUSIC_DIR = "$HOME/shelf/Music";
-            XDG_PICTURES_DIR = "$HOME/shelf/Pictures";
-            XDG_VIDEOS_DIR = "$HOME/shelf/Videos";
-            XDG_TEMPLATES_DIR = "$HOME/shelf/Templates";
-            XDG_PUBLICSHARE_DIR = "$HOME/shelf/Public";
-        };
     })
     (lib.mkIf config.mount-mirror.enable {
         fileSystems."/mirror" =
---
+++ home/common/global/config-folders.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+
+    xdg.userDirs = {
+        enable = true;
+        desktop = "${config.home.homeDirectory}/shelf/Desktop";
+        documents = "${config.home.homeDirectory}/shelf/Documents";
+        download = "${config.home.homeDirectory}/shelf/Downloads";
+        music = "${config.home.homeDirectory}/shelf/Music";
+        pictures = "${config.home.homeDirectory}/shelf/Pictures";
+        videos = "${config.home.homeDirectory}/shelf/Videos";
+        templates = "${config.home.homeDirectory}/shelf/Templates";
+        publicShare = "${config.home.homeDirectory}/shelf/Public";
+    };
+}
