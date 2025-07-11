10f85323afd9242bca2b08b5408f3c0caf66ae54
Temhr <9110264+Temhr@users.noreply.github.com>
2025-07-11 15:55:02 -0400

    Update auto-backup-home.sh

--- hosts/files/scripts/auto-backup-home.sh
+++ hosts/files/scripts/auto-backup-home.sh
@@ -10,21 +10,21 @@ baseDir="/mnt/mirk3"
 
 if [ -d "$mirrorDir" ]; then
     if [ -d "${mirrorDir}/home/${hostname}/" ]; then
-        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --copy-unsafe-links --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${mirrorDir}/home/${hostname}/ /home/temhr/ ${mirrorDir}/home/${hostname}/
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${mirrorDir}/home/${hostname}/ /home/temhr/ ${mirrorDir}/home/${hostname}/
     else
-        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --copy-unsafe-links --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${mirrorDir}/home/${hostname}/
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${mirrorDir}/home/${hostname}/
     fi
 elif [ -d "$serDir" ]; then
     if [ -d "${serDir}/home/${hostname}/" ]; then
-        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --copy-unsafe-links --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${serDir}/home/${hostname}/ /home/temhr/ ${serDir}/home/${hostname}/
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${serDir}/home/${hostname}/ /home/temhr/ ${serDir}/home/${hostname}/
     else
-        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --copy-unsafe-links --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${serDir}/home/${hostname}/
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${serDir}/home/${hostname}/
     fi
 else
     if [ -d "${baseDir}/home/${hostname}/" ]; then
-        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --copy-unsafe-links --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${baseDir}/home/${hostname}/ /home/temhr/ ${baseDir}/home/${hostname}/
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' --link-dest=${baseDir}/home/${hostname}/ /home/temhr/ ${baseDir}/home/${hostname}/
     else
-        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --copy-unsafe-links --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${baseDir}/home/${hostname}/
+        /run/wrappers/bin/sudo -u "temhr" /run/current-system/sw/bin/rsync -rva --delete --exclude '*cache*' --exclude '*Cache*' --exclude '*Trash*' /home/temhr/ ${baseDir}/home/${hostname}/
     fi
 fi
 
