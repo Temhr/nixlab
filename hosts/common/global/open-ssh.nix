9679305fdb747f68a97d48b2195fa9398c677614
Temhr <9110264+Temhr@users.noreply.github.com>
2025-07-16 21:05:41 -0400

    Update open-ssh.nix

--- hosts/common/global/open-ssh.nix
+++ hosts/common/global/open-ssh.nix
@@ -14,4 +14,9 @@
     };
   };
   programs.ssh.startAgent = true;
+  programs.ssh.knownHosts = {
+    "github.com" = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    };
+  };
 }
