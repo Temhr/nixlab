cbcce8978ce9aff0cafc58a603321f052a508f56
Temhr <9110264+Temhr@users.noreply.github.com>
2025-07-19 07:31:12 -0400

    pre-upgrade flake update

---
+++ lib/auto-nixos-upgrade.nix
@@ -0,0 +1,17 @@
+{ ... }: {
+  system.autoUpgrade = {
+    enable = true;
+    operation = "boot"; #switch or boot
+    flake = "github:Temhr/nixlab"; #Flake URI of the NixOS configuration to build
+    allowReboot = true;
+    randomizedDelaySec = "30m";
+    dates = "02:00";
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+      #"--commit-lock-file"
+      "--no-write-lock-file"
+    ];
+  };
+}
