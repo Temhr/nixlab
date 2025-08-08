077f47abd99b7fa5200f9a052b5f7b06bc8dbe2e
Temhr <9110264+Temhr@users.noreply.github.com>
2025-08-08 04:35:22 -0400

    Update ephemeral-apps.nix

--- home/common/global/ephemeral-apps.nix
+++ home/common/global/ephemeral-apps.nix
@@ -7,10 +7,10 @@
     ## Audacity
     ".local/share/applications/audacity.desktop".text = ''
       [Desktop Entry]
-      Name=Audacity
+      Name=Audacity ⚡
       Comment=Sound editor with graphical UI
       Exec=konsole -e nix run nixpkgs#audacity
-      Icon=utilities-terminal
+      Icon=audacity
       Terminal=false
       Type=Application
       Categories=AudioVideo;Audio;AudioVideoEditing;Ephemeral-App;
@@ -19,22 +19,22 @@
      ## Blender
     ".local/share/applications/blender-cuda.desktop".text = ''
       [Desktop Entry]
-      Name=Blender (CUDA)
-      Comment=3D Creation/Animation/Publishing System (with CUDA support)
+      Name=Blender CUDA ⚡
+      Comment=3D Creation/Animation/Publishing System
       Exec=env NIXPKGS_ALLOW_UNFREE=1 nix run --impure --expr "(import <nixpkgs> {}).blender.override {cudaSupport=true;}"
       Icon=blender
       Terminal=false
       Type=Application
-      Categories=Graphics;3DGraphics;
+      Categories=Graphics;3DGraphics;Ephemeral-App;
     '';
 
     /* ## darktable - doesn't work properly because dependency libsoup-2.74.3 is a security risk
     ".local/share/applications/darktable.desktop".text = ''
       [Desktop Entry]
-      Name=Darktable
-      Comment=Virtual lighttable and darkroom for photographers
+      Name=Darktable ⚡
+      Comment=Virtual lighttable and darkroom
       Exec=konsole -e nix run nixpkgs#darktable
-      Icon=utilities-terminal
+      Icon=darktable
       Terminal=false
       Type=Application
       Categories=Graphics;Photography;RasterGraphics;Ephemeral-App;
@@ -43,10 +43,10 @@
      ## Davinci Resolve
     ".local/share/applications/davinci-resolve.desktop".text = ''
       [Desktop Entry]
-      Name=Davinci Resolve
-      Comment=Professional video editing, color, effects and audio post-processing
+      Name=DaVinci Resolve ⚡
+      Comment=Professional video editing, color, effects and audio
       Exec=konsole -e NIXPKGS_ALLOW_UNFREE=1 nix run --impure nixpkgs#davinci-resolve
-      Icon=utilities-terminal
+      Icon=DaVinciResolve
       Terminal=false
       Type=Application
       Categories=AudioVideo;Video;AudioVideoEditing;Ephemeral-App;
@@ -55,10 +55,10 @@
     ## Gimp
     ".local/share/applications/gimp.desktop".text = ''
       [Desktop Entry]
-      Name=Gimp
+      Name=GIMP ⚡
       Comment=GNU Image Manipulation Program
       Exec=konsole -e nix run nixpkgs#gimp3-with-plugins
-      Icon=utilities-terminal
+      Icon=gimp
       Terminal=false
       Type=Application
       Categories=Graphics;RasterGraphics;2DGraphics;Ephemeral-App;
@@ -67,10 +67,10 @@
     ## Inkscape
     ".local/share/applications/inkscape.desktop".text = ''
       [Desktop Entry]
-      Name=Inkscape
+      Name=Inkscape ⚡
       Comment=Vector graphics editor
       Exec=konsole -e nix run nixpkgs#inkscape-with-extensions
-      Icon=utilities-terminal
+      Icon=inkscape
       Terminal=false
       Type=Application
       Categories=Graphics;VectorGraphics;2DGraphics;Ephemeral-App;
@@ -79,10 +79,10 @@
     ## Kdenlive
     ".local/share/applications/kdenlive.desktop".text = ''
       [Desktop Entry]
-      Name=Kdenlive
-      Comment=Free and open source video editor, based on MLT Framework and KDE Frameworks
+      Name=Kdenlive ⚡
+      Comment=Video editor based on MLT Framework
       Exec=konsole -e nix run nixpkgs#kdePackages.kdenlive
-      Icon=utilities-terminal
+      Icon=kdenlive
       Terminal=false
       Type=Application
       Categories=AudioVideo;Video;AudioVideoEditing;Ephemeral-App;
@@ -91,10 +91,10 @@
     ## Krita
     ".local/share/applications/krita.desktop".text = ''
       [Desktop Entry]
-      Name=Krita
+      Name=Krita ⚡
       Comment=Free and open source painting application
       Exec=konsole -e nix run nixpkgs#krita
-      Icon=utilities-terminal
+      Icon=krita
       Terminal=false
       Type=Application
       Categories=Graphics;2DGraphics;Painting;Ephemeral-App;
@@ -103,10 +103,10 @@
     ## Lutris
     ".local/share/applications/lutris.desktop".text = ''
       [Desktop Entry]
-      Name=Lutris
+      Name=Lutris ⚡
       Comment=Open Source gaming platform for GNU/Linux
       Exec=konsole -e NIXPKGS_ALLOW_UNFREE=1 nix run --impure nixpkgs#lutris
-      Icon=utilities-terminal
+      Icon=lutris
       Terminal=false
       Type=Application
       Categories=Game;Ephemeral-App;
@@ -115,10 +115,10 @@
     ## Media-downloader
     ".local/share/applications/media-downloader.desktop".text = ''
       [Desktop Entry]
-      Name=Media-downloader
-      Comment=Qt/C++ GUI front end for yt-dlp and others
+      Name=Media Downloader ⚡
+      Comment=Qt/C++ GUI front end for yt-dlp
       Exec=konsole -e nix run nixpkgs#media-downloader
-      Icon=utilities-terminal
+      Icon=media-downloader
       Terminal=false
       Type=Application
       Categories=Network;FileTransfer;Video;Ephemeral-App;
@@ -127,10 +127,10 @@
     ## Openshot
     ".local/share/applications/openshot.desktop".text = ''
       [Desktop Entry]
-      Name=Openshot
+      Name=OpenShot ⚡
       Comment=Free, open-source video editor
       Exec=konsole -e nix run nixpkgs#openshot-qt
-      Icon=utilities-terminal
+      Icon=openshot-qt
       Terminal=false
       Type=Application
       Categories=AudioVideo;Video;AudioVideoEditing;Ephemeral-App;
@@ -139,10 +139,10 @@
     ## SuperTuxKart
     ".local/share/applications/superTuxKart.desktop".text = ''
       [Desktop Entry]
-      Name=SuperTuxKart
+      Name=SuperTuxKart ⚡
       Comment=A Free 3D kart racing game
       Exec=konsole -e nix run nixpkgs#superTuxKart
-      Icon=utilities-terminal
+      Icon=supertuxkart
       Terminal=false
       Type=Application
       Categories=Game;Ephemeral-App;
