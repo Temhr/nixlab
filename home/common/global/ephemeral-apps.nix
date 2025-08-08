05768a75b13f29604738d06e38381c9641032510
Temhr <9110264+Temhr@users.noreply.github.com>
2025-08-08 14:01:48 -0400

    Update ephemeral-apps.nix

--- home/common/global/ephemeral-apps.nix
+++ home/common/global/ephemeral-apps.nix
@@ -9,7 +9,7 @@
       [Desktop Entry]
       Name=Audacity ⚡
       Comment=Sound editor with graphical UI
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#audacity; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#audacity; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=audacity
       Terminal=false
       Type=Application
@@ -21,7 +21,7 @@
       [Desktop Entry]
       Name=Blender CUDA ⚡
       Comment=3D Creation/Animation/Publishing System
-      Exec=konsole -e bash -c "echo 'Launching...'; NIXPKGS_ALLOW_UNFREE=1 nix run --impure --expr '(import <nixpkgs> {}).blender.override {cudaSupport=true;}'; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; NIXPKGS_ALLOW_UNFREE=1 nix run --impure --expr '(import <nixpkgs> {}).blender.override {cudaSupport=true;}'; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=blender
       Terminal=false
       Type=Application
@@ -33,7 +33,7 @@
       [Desktop Entry]
       Name=Darktable ⚡
       Comment=Virtual lighttable and darkroom
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#darktable; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#darktable; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=darktable
       Terminal=false
       Type=Application
@@ -45,7 +45,7 @@
       [Desktop Entry]
       Name=DaVinci Resolve ⚡
       Comment=Professional video editing, color, effects and audio
-      Exec=konsole -e bash -c "echo 'Launching...'; NIXPKGS_ALLOW_UNFREE=1 nix run --impure nixpkgs#davinci-resolve; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; NIXPKGS_ALLOW_UNFREE=1 nix run --impure nixpkgs#davinci-resolve; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=DaVinciResolve
       Terminal=false
       Type=Application
@@ -57,7 +57,7 @@
       [Desktop Entry]
       Name=GIMP ⚡
       Comment=GNU Image Manipulation Program
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#gimp3-with-plugins; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#gimp3-with-plugins; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=gimp
       Terminal=false
       Type=Application
@@ -69,7 +69,7 @@
       [Desktop Entry]
       Name=Inkscape ⚡
       Comment=Vector graphics editor
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#inkscape-with-extensions; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#inkscape-with-extensions; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=inkscape
       Terminal=false
       Type=Application
@@ -81,7 +81,7 @@
       [Desktop Entry]
       Name=Kdenlive ⚡
       Comment=Video editor based on MLT Framework
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#kdePackages.kdenlive; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#kdePackages.kdenlive; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=kdenlive
       Terminal=false
       Type=Application
@@ -93,7 +93,7 @@
       [Desktop Entry]
       Name=Krita ⚡
       Comment=Free and open source painting application
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#krita; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#krita; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=krita
       Terminal=false
       Type=Application
@@ -105,7 +105,7 @@
       [Desktop Entry]
       Name=Lutris ⚡
       Comment=Open Source gaming platform for GNU/Linux
-      Exec=konsole -e bash -c "echo 'Launching...'; NIXPKGS_ALLOW_UNFREE=1 nix run --impure nixpkgs#lutris; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; NIXPKGS_ALLOW_UNFREE=1 nix run --impure nixpkgs#lutris; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=lutris
       Terminal=false
       Type=Application
@@ -117,7 +117,7 @@
       [Desktop Entry]
       Name=Media Downloader ⚡
       Comment=Qt/C++ GUI front end for yt-dlp
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#media-downloader; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#media-downloader; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=media-downloader
       Terminal=false
       Type=Application
@@ -129,7 +129,7 @@
       [Desktop Entry]
       Name=OpenShot ⚡
       Comment=Free, open-source video editor
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#openshot-qt; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#openshot-qt; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=openshot-qt
       Terminal=false
       Type=Application
@@ -141,7 +141,7 @@
       [Desktop Entry]
       Name=SuperTuxKart ⚡
       Comment=A Free 3D kart racing game
-      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#superTuxKart; echo 'Press Enter to close terminal...'; read"
+      Exec=konsole -e bash -c "echo 'Launching...'; nix run nixpkgs#superTuxKart; echo; echo 'Terminal will close in 3 seconds (Press Enter to pause timer)...'; if timeout 3 bash -c 'read -r'; then echo 'Timer paused. Press Enter to close...'; read -r; fi"
       Icon=supertuxkart
       Terminal=false
       Type=Application
