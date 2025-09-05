68a730e2ce285edf15bd0ddabdd1357bd14a4074
Temhr <9110264+Temhr@users.noreply.github.com>
2025-09-05 05:02:21 -0400

    Update auto-system-reboot.nix

--- hosts/common/global/auto-system-reboot.nix
+++ hosts/common/global/auto-system-reboot.nix
@@ -41,7 +41,7 @@ in {
     description = "Timer for Conditional Random Reboot";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "04:00..04:59";
+      OnCalendar = "05:00..05:59";
       Persistent = true;
     };
   };
