335d1a6ee5aaad4060fdbffa74b5ffd193448783
Temhr <9110264+Temhr@users.noreply.github.com>
2025-09-17 21:08:02 -0400

    Update repast.nix

--- shells/repast.nix
+++ shells/repast.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 
-pkgs.mkShell {
-  buildInputs = with pkgs; [
+(pkgs.buildFHSUserEnv {
+  name = "repast";
+  targetPkgs = pkgs: with pkgs; [
     python3
     python3Packages.pip
     python3Packages.virtualenv
@@ -11,21 +12,27 @@ pkgs.mkShell {
     python3Packages.wheel
     openmpi
     gcc
+    glibc
     glibc.dev
     stdenv.cc.cc
     stdenv.cc.cc.lib
-    pkg-config
+    zlib
     zlib.dev
+    pkg-config
   ];
 
-  shellHook = ''
-    export CC=${pkgs.gcc}/bin/gcc
-    export CXX=${pkgs.gcc}/bin/g++
+  multiPkgs = pkgs: with pkgs; [
+    zlib
+  ];
+
+  runScript = "bash";
+
+  profile = ''
+    export CC=gcc
+    export CXX=g++
     export CFLAGS="-I${pkgs.glibc.dev}/include"
     export CXXFLAGS="-I${pkgs.glibc.dev}/include"
     export LDFLAGS="-L${pkgs.glibc}/lib -L${pkgs.stdenv.cc.cc.lib}/lib"
-    export LD_LIBRARY_PATH="${pkgs.glibc}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.openmpi}/lib:$LD_LIBRARY_PATH"
-    export PKG_CONFIG_PATH="${pkgs.openmpi}/lib/pkgconfig:$PKG_CONFIG_PATH"
-    echo "Development environment with C/C++ headers ready"
+    export LD_LIBRARY_PATH="${pkgs.glibc}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
   '';
-}
+}).env
