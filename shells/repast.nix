68a672e6fde1059d92130e3dbc6a7da309d34977
Temhr <9110264+Temhr@users.noreply.github.com>
2025-09-18 06:10:13 -0400

    Update repast.nix

--- shells/repast.nix
+++ shells/repast.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
-(pkgs.buildFHSUserEnv {
-  name = "repast";
+(pkgs.buildFHSEnv {
+  name = "repast4py-dev";
   targetPkgs = pkgs: with pkgs; [
     python3
     python3Packages.pip
@@ -12,17 +12,21 @@
     python3Packages.wheel
     openmpi
     gcc
+    binutils
     glibc
     glibc.dev
+    glibc.static
     stdenv.cc.cc
     stdenv.cc.cc.lib
     zlib
     zlib.dev
     pkg-config
+    libgcc
   ];
 
   multiPkgs = pkgs: with pkgs; [
     zlib
+    glibc
   ];
 
   runScript = "bash";
@@ -30,9 +34,14 @@
   profile = ''
     export CC=gcc
     export CXX=g++
-    export CFLAGS="-I${pkgs.glibc.dev}/include"
-    export CXXFLAGS="-I${pkgs.glibc.dev}/include"
-    export LDFLAGS="-L${pkgs.glibc}/lib -L${pkgs.stdenv.cc.cc.lib}/lib"
+    # Use FHS-style paths that the linker expects
+    export LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu:${pkgs.glibc}/lib:${pkgs.stdenv.cc.cc.lib}/lib"
+    export C_INCLUDE_PATH="/usr/include:${pkgs.glibc.dev}/include"
+    export CPLUS_INCLUDE_PATH="/usr/include:${pkgs.glibc.dev}/include"
     export LD_LIBRARY_PATH="${pkgs.glibc}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+    echo "Repast4Py development environment ready!"
+    echo "Create virtual environment: python -m venv repast-env"
+    echo "Activate it: source repast-env/bin/activate"
+    echo "Install repast4py: pip install repast4py"
   '';
 }).env
