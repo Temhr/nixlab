f99e4edae3faf1adb67030721393ae4736268d04
Temhr <9110264+Temhr@users.noreply.github.com>
2025-09-17 20:53:03 -0400

    Update repast.nix

--- shells/repast.nix
+++ shells/repast.nix
@@ -1,23 +1,25 @@
 { pkgs, ... }:
 
 pkgs.mkShell {
-    name = "repast";
-    buildInputs = with pkgs; [
-        python3
-        python3Packages.pip
-        python3Packages.virtualenv
-        python3Packages.numpy
-        python3Packages.mpi4py
-        python3Packages.networkx
-        python3Packages.matplotlib
-        openmpi
-        gcc
-        pkg-config
-    ];
+  buildInputs = with pkgs; [
+    python3
+    python3Packages.pip
+    python3Packages.virtualenv
+    python3Packages.numpy
+    python3Packages.mpi4py
+    python3Packages.networkx
+    openmpi
+    gcc
+    pkg-config
+    stdenv.cc.cc.lib
+    glibc
+    zlib
+  ];
 
-    shellHook = ''
-        export LD_LIBRARY_PATH=${pkgs.openmpi}/lib:$LD_LIBRARY_PATH
-        export PYTHONPATH=""
-        echo "Python development environment for Repast4Py ready"
-    '';
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.glibc}/lib:${pkgs.openmpi}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH"
+    export CC=${pkgs.gcc}/bin/gcc
+    export CXX=${pkgs.gcc}/bin/g++
+    echo "Enhanced Repast4Py development environment ready"
+  '';
 }
