dcba12a7a3d96d05d834bcf032ffc159feaabd4e
Temhr <9110264+Temhr@users.noreply.github.com>
2025-10-13 08:11:03 -0400

    Update configuration.nix

--- lib/init-nixos/configuration.nix
+++ lib/init-nixos/configuration.nix
@@ -37,8 +37,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  #services.displayManager.gdm.enable = true;
+  #services.desktopManager.gnome.enable = true;
   # Enable Plasma6
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
