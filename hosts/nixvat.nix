1d4884924fed4a667446be770457e6bd7026dec0
Temhr <9110264+Temhr@users.noreply.github.com>
2025-11-25 13:59:51 -0500

    Update nixvat.nix

--- hosts/nixvat.nix
+++ hosts/nixvat.nix
@@ -78,13 +78,11 @@
   };
 
   services.wikijs-custom = {
-    enable = true;
-    port = 3001;
-    bindIP = "0.0.0.0";
-    openFirewall = true;
-    dataDir = "/data/wiki-js";
-    databaseType = "postgres";
-    autoSetupPostgres = true;
+    enable = true;                # REQUIRED: Turn on the service
+    port = 3001;                  # OPTIONAL: Default is 3001
+    bindIP = "127.0.0.1";        # OPTIONAL: Default is 127.0.0.1
+    dataDir = "/data/wiki-js";    # OPTIONAL: Default is /var/lib/wiki-js
+    openFirewall = true;          # Default is true
   };
 
   services.bookstack-custom = {
