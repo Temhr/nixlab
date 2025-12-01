fa303266d18f3a9c0d809cdc4d9f0d81c4478819
Temhr <9110264+Temhr@users.noreply.github.com>
2025-12-01 06:12:57 -0500

    Update nixtop.nix

--- hosts/nixtop.nix
+++ hosts/nixtop.nix
@@ -70,20 +70,6 @@
   virtualisation.waydroid.enable = true; #requires "$sudo waydroid init" with "-s GAPPS -f" flag option
 
   ## Self-hosted apps and services
-  services.ollama-cpu = {
-    enable = true;
-    ollamaPort = 11434;
-    webuiPort = 3006;
-    ollamaBindIP = "0.0.0.0";
-    webuiBindIP = "0.0.0.0";
-    ollamaDataDir = "/data/ollama";
-    webuiDataDir = "/data/open-webui";
-
-    # Pre-download models
-    models = [ "llama2" "mistral" "codellama" ];
-
-    openFirewall = true;
-  };
 
   services.grafana-custom = {
     enable = true;
