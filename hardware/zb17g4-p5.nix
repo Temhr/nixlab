6187ab458d130b9896b4be1e9efbd03d5e6c2133
Effy <9110264+Temhr@users.noreply.github.com>
2025-12-29 19:14:29 -0500

    Enable home drive mounting in zb17g4-p5 configuration

--- hardware/zb17g4-p5.nix
+++ hardware/zb17g4-p5.nix
@@ -9,7 +9,7 @@
   # Choose between these choices: "none" "k" "p"
   driver-nvidia.quadro = "p";
 
-  #mount-home.enable = true; #mounts home drive
+  mount-home.enable = true; #mounts home drive
   mount-shelf.enable = true; #mounts shelf drive in home directory
   #mount-mirror.enable = true; #mounts mirror drive
   mount-mirk1.enable = true; #mounts mirk1 nfs
