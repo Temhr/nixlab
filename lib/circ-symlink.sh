6039b3982182e25ef1d918e9fc3e2151c11c3b01
Temhr <9110264+Temhr@users.noreply.github.com>
2026-01-09 04:05:09 -0500

    Delete circ-symlink.sh

--- lib/circ-symlink.sh
+++
@@ -1,8 +0,0 @@
-find /home/temhr/ -type l -exec bash -c '
-    for link; do
-        target=$(readlink -f "$link" 2>/dev/null)
-        if [[ "$target" == "$link"* ]] || [[ "$link" == "$target"* ]]; then
-            echo "Circular symlink: $link -> $(readlink "$link")"
-        fi
-    done
-' bash {} +
