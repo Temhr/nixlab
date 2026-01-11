24461f48e81bfef1ecfb1813c140a87f8633280d
Effy <9110264+Temhr@users.noreply.github.com>
2026-01-10 22:03:27 -0500

    Disable mirk1 and mirk3 NFS mounts
    Comment out mirk1 and mirk3 NFS mounts

--- hardware/zb17g2-k5.nix
+++ hardware/zb17g2-k5.nix
@@ -14,7 +14,7 @@
   mount-home.enable = true; #mounts home drive
   mount-shelf.enable = true; #mounts shelf drive in home directory
   #mount-mirror.enable = true; #mounts mirror drive
-  mount-mirk1.enable = true; #mounts mirk1 nfs
-  mount-mirk3.enable = true; #mounts mirk3 nfs
+  #mount-mirk1.enable = true; #mounts mirk1 nfs
+  #mount-mirk3.enable = true; #mounts mirk3 nfs
 
 }
