ba5e8f809996ef2424040b58ac6e6958b45c2baa
Temhr <temhr4@gmail.com>
2026-03-25 12:00:32 -0400

    fixes

--- flake/parts/overlays.nix
+++ flake/parts/overlays.nix
@@ -1,19 +1,15 @@
 {inputs, ...}: {
-  flake.overlays =
-    import ../../overlays {inherit inputs;};
-  flake.nixosModules.nixlab =
-    import ../../modules/nixos;
+  flake.overlays = import ../../overlays {inherit inputs;};
+  flake.nixosModules = {
+    nixlab = import ../../modules/nixos;
+  };
   flake.homeModules = {
     # Common layers
-    home-common-global =
-      import ../../home/common/global;
-    home-common-optional =
-      import ../../home/common/optional;
+    home-common-global = import ../../home/common/global;
+    home-common-optional = import ../../home/common/optional;
     # Module library
-    browsers =
-      import ../../modules/home-manager/browsers.nix;
-    terminal-emulators =
-      import ../../modules/home-manager/terminal-emulators.nix;
+    browsers = import ../../modules/home-manager/browsers.nix;
+    terminal-emulators = import ../../modules/home-manager/terminal-emulators.nix;
     # Per-host user configs
     temhr-nixace = import ../../home/temhr/nixace.nix;
     temhr-nixsun = import ../../home/temhr/nixsun.nix;
