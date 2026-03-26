e5d16a268e8f0646a3c4a3b879e294a3e2d806f7
Temhr <temhr4@gmail.com>
2026-03-26 09:40:00 -0400

    rm empty option & config blocks

--- hosts/common/optional/default.nix
+++ hosts/common/optional/default.nix
@@ -12,18 +12,4 @@
     ./productivity.nix
     ./virtualizations.nix
   ];
-
-  options = {
-    # Option declarations.
-    # Declare what settings a user of this module can set.
-    # Usually this includes a global "enable" option which defaults to false.
-  };
-
-  config = {
-    # Option definitions.
-    # Define what other settings, services and resources should be active.
-    # Usually these depend on whether a user of this module chose to "enable" it
-    # using the "option" above.
-    # Options for modules imported in "imports" can be set here.
-  };
 }
