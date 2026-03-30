1be9a0e2e668167e375bf17fb2c676db28f932a8
Temhr <temhr4@gmail.com>
2026-03-30 18:51:46 -0400

    declare homeModules as a custom mergeable option

---
+++ flake/parts/home-options.nix
@@ -0,0 +1,7 @@
+{lib, ...}: {
+  options.flake.homeModules = lib.mkOption {
+    type = lib.types.lazyAttrsOf lib.types.raw;
+    default = {};
+    description = "Home Manager modules exported by this flake.";
+  };
+}
