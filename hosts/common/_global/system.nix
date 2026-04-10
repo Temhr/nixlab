130332fcacac8b3742049dd07d2da0ee992ac8df
Temhr <9110264+Temhr@users.noreply.github.com>
2026-04-10 07:57:42 -0400

    reset

--- hosts/common/_global/system.nix
+++ hosts/common/_global/system.nix
@@ -1,14 +1,4 @@
 {...}: {
 
-    systemd.user.units."drkonqi-coredump-pickup.service" = {
-        text = ''
-            [Unit]
-            Description=Masked
-
-            [Service]
-            ExecStart=/bin/true
-            Type=oneshot
-        '';
-    };
 
 }
