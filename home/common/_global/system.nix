e769a3d5eb99ea0ea7a972fa9fef23681256e6d7
Temhr <9110264+Temhr@users.noreply.github.com>
2026-04-10 08:08:19 -0400

    mv

--- home/common/_global/system.nix
+++ home/common/_global/system.nix
@@ -1,7 +1,5 @@
 {...}: {
-systemd.services.home-manager-temhr.serviceConfig.Environment = [
-  "XDG_RUNTIME_DIR=/run/user/%U"  # %U expands to the user's UID
-];
+
   systemd.user.startServices = "suggest";
 
 }
--- hosts/common/_global/system.nix
+++ hosts/common/_global/system.nix
@@ -1,4 +1,6 @@
 {...}: {
 
-
+systemd.services.home-manager-temhr.serviceConfig.Environment = [
+  "XDG_RUNTIME_DIR=/run/user/%U"  # %U expands to the user's UID
+];
 }
