a6e5914dc5b40e6c2100dcbe0e619fb5669732f5
Temhr <9110264+Temhr@users.noreply.github.com>
2026-04-22 10:36:15 -0400

    fix

--- hardware/m720q-nas1.nix
+++ hardware/m720q-nas1.nix
@@ -16,7 +16,6 @@
         "/dev/disk/by-id/scsi-35000cca073b28880"
         "/dev/disk/by-id/scsi-35000cca073b23fdc"
       ];
-      mountPoint = "/zpool";
       enableMonitoring = true;
       #alertEmail = "your-email@example.com";  # Optional: for email alerts
     };
