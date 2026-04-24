f8bf69eaf5674273db9f48ba533a02420a694d74
Temhr <temhr4@gmail.com>
2026-04-24 12:07:08 -0400

    edit

--- overlays/_ollama-p5000.nix
+++ overlays/_ollama-p5000.nix
@@ -21,7 +21,7 @@ in {
     }).overrideAttrs (old: {
       pname = "ollama-cuda-p5000";
       name = "ollama-cuda-p5000-${old.version}";
-      vendorHash = "";
+      vendorHash = "sha256-Lc1Ktdqtv2VhJQssk8K1UOimeEjVNvDWePE9WkamCos=";
     });
 
   ollama = stable.ollama;
