3836e2e5493a249a67a7701b061ac0f22e53e2a1
Temhr <temhr4@gmail.com>
2026-04-26 06:56:54 -0400

    edits

--- modules/nixos/cachix.nix
+++ modules/nixos/cachix.nix
@@ -1,12 +1,26 @@
 # WARN: this file will get overwritten by $ cachix use <name>
 {...}: {
-  flake.nixosModules.systm--cachix = {lib, ...}: let
-    folder = ../../cachix;
+  flake.nixosModules.systm--cachix = {
+    flakePath,
+    lib,
+    ...
+  }: let
+    folder = flakePath + /cachix;
     toImport = name: _: folder + ("/" + name);
     filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
-    imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
+    imports =
+      if builtins.pathExists folder
+      then lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder))
+      else [];
   in {
     inherit imports;
-    nix.settings.substituters = ["https://cache.nixos.org/"];
+
+    # Default Nix cache settings
+    nix.settings = {
+      substituters = ["https://cache.nixos.org"];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+    };
   };
 }
