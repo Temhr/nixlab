dac43710bdd35f2487a25a72d572277a33531a4e
Temhr <temhr4@gmail.com>
2026-04-29 19:12:24 -0400

    fix

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     # import-tree recursively discovers every .nix file
     import-tree.url = "github:vic/import-tree";
 
-    # Version Pinned Apps (2026-04-22).
+    # Version Pinned Apps (2026-04-27).
     # nix flake metadata nixpkgs-unstable | grep "Revision"
     nixpkgs-ollama.url = "github:nixos/nixpkgs/1c3fe55ad329cbcb28471bb30f05c9827f724c76";
     # ComfyUI Pin is in nixlab/overlays/_comfyui-p5000.nix
