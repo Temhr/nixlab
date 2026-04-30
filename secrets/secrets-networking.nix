a860746cef828ed98a776ffc0ebbd0b3afefafc3
Temhr <temhr4@gmail.com>
2026-04-30 08:47:27 -0400

    to sops/

--- secrets/secrets-networking.nix
+++
@@ -1,6 +0,0 @@
-{...}: {
-  flake.nixosModules.nsops--networking = {...}: {
-    sops.secrets.wifi_ssid = {sopsFile = ./networking.yaml;};
-    sops.secrets.wifi_password = {sopsFile = ./networking.yaml;};
-  };
-}
