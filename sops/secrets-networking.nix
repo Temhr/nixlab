d6a8f4a5fcbc30dade58b8136ba7ed9668439ef6
Temhr <temhr4@gmail.com>
2026-05-10 14:13:32 -0400

    chown networking file

--- sops/secrets-networking.nix
+++ sops/secrets-networking.nix
@@ -1,6 +1,14 @@
 {...}: {
   flake.nixosModules.nsops--networking = {...}: {
-    sops.secrets.wifi_ssid = {sopsFile = ./networking.yaml;};
-    sops.secrets.wifi_password = {sopsFile = ./networking.yaml;};
+    sops.secrets.wifi_ssid = {
+      sopsFile = ./networking.yaml;
+      owner = "root";
+      group = "root";
+    };
+    sops.secrets.wifi_password = {
+      sopsFile = ./networking.yaml;
+      owner = "root";
+      group = "root";
+    };
   };
 }
