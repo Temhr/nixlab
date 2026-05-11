1bfa4313db6a7c62c01d9a400ab227366540c9f2
Effy <9110264+Temhr@users.noreply.github.com>
2026-05-11 07:04:54 -0400

    Uncomment owner, group, and restartUnits for secrets

--- sops/secrets-networking.nix
+++ sops/secrets-networking.nix
@@ -2,15 +2,15 @@
   flake.nixosModules.nsops--networking = {...}: {
     sops.secrets.wifi_ssid = {
       sopsFile = ./networking.yaml;
-      #owner = "root";
-      #group = "root";
-      #restartUnits = ["NetworkManager.service"];
+      owner = "root";
+      group = "root";
+      restartUnits = ["NetworkManager.service"];
     };
     sops.secrets.wifi_password = {
       sopsFile = ./networking.yaml;
-      #owner = "root";
-      #group = "root";
-      #restartUnits = ["NetworkManager.service"];
+      owner = "root";
+      group = "root";
+      restartUnits = ["NetworkManager.service"];
     };
   };
 }
