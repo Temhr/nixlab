cb5add4137d397cc67d20af2f41dc505530d6a33
Temhr <temhr4@gmail.com>
2026-05-14 10:13:11 -0400

    rv unstable

--- flake/parts/_hosts-meta.nix
+++ flake/parts/_hosts-meta.nix
@@ -39,7 +39,7 @@ in {
     ethIface = "eno1";
     wifiIface = "wlp1s0";
     hostId = "23d031fa";
-    nixpkgsInput = "nixpkgs-unstable";
+    nixpkgsInput = "nixpkgs-stable";
     services = ["glance" "grafana" "prometheus" "loki"];
   };
   nixace = mkHostMeta {
