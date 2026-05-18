0d3db611a2352f36acccdbb7a2c847516a59a84b
Temhr <temhr4@gmail.com>
2026-05-17 21:36:30 -0400

    add syncthing widget

--- flake/parts/_hosts-meta.nix
+++ flake/parts/_hosts-meta.nix
@@ -40,7 +40,7 @@ in {
     wifiIface = "wlp1s0";
     hostId = "23d031fa";
     nixpkgsInput = "nixpkgs-stable";
-    services = ["glance" "grafana" "prometheus" "loki"];
+    services = ["glance" "grafana" "prometheus" "loki" "syncthing"];
   };
   nixace = mkHostMeta {
     address = "192.168.0.200";
@@ -72,7 +72,7 @@ in {
     wifiIface = "wlo1";
     hostId = "5845aa8d";
     nixpkgsInput = "nixpkgs-stable";
-    services = ["glance" "grafana" "prometheus" "loki" "ollama-cpu" "syncthing" "wikijs" "zola"];
+    services = ["glance" "grafana" "prometheus" "loki" "ollama-cpu" "wikijs" "zola" "syncthing"];
   };
   nixzen = mkHostMeta {
     address = "192.168.0.204";
