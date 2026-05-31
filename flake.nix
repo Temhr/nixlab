1e268099ddd26bd1a52371ad15b67f61a8e75694
Temhr <temhr4@gmail.com>
2026-05-31 11:05:28 -0400

    migrate flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   inputs = {
     # Core NixOS package collections (25.11 as default)
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
