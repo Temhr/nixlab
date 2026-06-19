9b9ec39004e42d7b455afb5107284dc0e6ed235d
Temhr <temhr4@gmail.com>
2026-06-19 04:46:37 -0400

    fix v2

--- hosts/common/hardware/-power-management.nix
+++ hosts/common/hardware/-power-management.nix
@@ -1,10 +1,9 @@
 {self, ...}: {
   flake.nixosModules.hosts--hardw--power-management = {pkgs, ...}: {
-    # Disable WD HDD power management (conditional on drive existence)
     systemd.services.disable-hdd-apm = {
       description = "Disable APM on data drive";
       wantedBy = ["multi-user.target"];
-      before = ["local-fs.target"];
+      after = ["multi-user.target"];
       unitConfig = {
         ConditionPathExists = "/dev/disk/by-label/data";
       };
@@ -13,14 +12,8 @@
         ExecStart = "${pkgs.hdparm}/bin/hdparm -B 255 -S 0 /dev/disk/by-label/data";
         RemainAfterExit = true;
       };
-      # Fix: remove any reference to sysinit.target or local-fs.target
-      after = [ "multi-user.target" ];   # was likely "sysinit.target"
-      wantedBy = [ "multi-user.target" ];  # not "sysinit.target"
     };
-
     boot.kernelParams = ["ahci.mobile_lpm_policy=1"];
-
-    # Increase RAM cache for filesystem
     boot.kernel.sysctl = {
       "vm.dirty_ratio" = 60;
       "vm.dirty_background_ratio" = 40;
