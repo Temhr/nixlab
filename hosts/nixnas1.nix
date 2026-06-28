6443da5f24dcc58c81200b217850a02558f7320b
Temhr <9110264+Temhr@users.noreply.github.com>
2026-06-28 11:04:34 -0400

    fix

--- hosts/nixnas1.nix
+++ hosts/nixnas1.nix
@@ -58,14 +58,14 @@
           path = "/mirror/phshare/photos";
           id = "Phone media";
           label = "Phone media";
-          devices = ["Pixel XL" "Pixel 3a XL"];
+          devices = ["Pixel 8 Pro" "Pixel 3a XL"];
           type = "sendreceive"; # or "sendonly" or "receiveonly" or "sendreceive"
         };
         "Docs" = {
           path = "/mirror/phshare/docs";
           id = "Docs";
           label = "Docs";
-          devices = ["Pixel XL" "Pixel 3a XL"];
+          devices = ["Pixel 8 Pro" "Pixel 3a XL"];
           type = "sendreceive"; # or "sendonly" or "receiveonly" or "sendreceive"
         };
       };
