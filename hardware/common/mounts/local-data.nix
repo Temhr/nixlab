3832e103859d29567691dd621cda75e653311050
Temhr <9110264+Temhr@users.noreply.github.com>
2026-08-17 09:49:07 -0400

    no fail

--- hardware/common/mounts/local-data.nix
+++ hardware/common/mounts/local-data.nix
@@ -3,7 +3,7 @@
     fileSystems."/data" = {
       device = "/dev/disk/by-label/data";
       fsType = "ext4";
-      options = ["defaults" "auto"];
+      options = ["defaults" "auto" "nofail" "x-systemd.device-timeout=10s"];
     };
   };
 }
