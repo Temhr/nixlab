e1a2784fc581b4d8c64e6f27912aa1589a193b7c
Temhr <temhr4@gmail.com>
2026-09-06 12:35:46 -0400

    update formatting

--- hosts/common/core/-journald.nix
+++ hosts/common/core/-journald.nix
@@ -1,10 +1,10 @@
 {...}: {
   flake.nixosModules.hosts--core--journald = {...}: {
-    services.journald.extraConfig = ''
-      SystemMaxUse=100M
-      SystemMaxFileSize=10M
-      SystemMaxFiles=10
-      MaxRetentionSec=1month
-    '';
+    services.journald.settings.Journal = {
+      SystemMaxUse = "100M";
+      SystemMaxFileSize = "10M";
+      SystemMaxFiles = 10;
+      MaxRetentionSec = "1month";
+    };
   };
 }
