82e78bdd81555bde50f17de25dc53b3da7e6e707
Temhr <temhr4@gmail.com>
2026-09-18 20:01:57 -0400

    rv fix

--- flake/builders/hardware.nix
+++ flake/builders/hardware.nix
@@ -22,7 +22,7 @@ in {
             fsType = "ext4";
           };
           fileSystems."/boot" = {
-            device = "/dev/disk/by-label/BOOT";
+            device = "/dev/disk/by-label/boot";
             fsType = "vfat";
             options = ["fmask=0077" "dmask=0077"];
           };
