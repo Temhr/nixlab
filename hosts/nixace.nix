b15f0569423930af5aa65d924fe06be5b087ce89
Effy <9110264+Temhr@users.noreply.github.com>
2026-09-18 20:19:41 -0400

    Update journald and nix module references

--- hosts/nixace.nix
+++ hosts/nixace.nix
@@ -7,8 +7,8 @@
       # Host config
       self.nixosModules.hosts--nixace
       self.nixosModules.hosts--profl--base
-      self.nixosModules.hosts--core--journald-old
-      self.nixosModules.hosts--core--nix-old
+      self.nixosModules.hosts--core--journald
+      self.nixosModules.hosts--core--nix
       self.nixosModules.hosts--profl--desktop
       # Services
       self.nixosModules.nsops--bookstack
